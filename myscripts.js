fac261969a3f0c42b52f9cfd0bb22538efb84465
Nick Guttilla <nguttilla01@manhattan.edu>
2020-04-17 20:00:04 -0400

    Changed If Statement to a Switch on the play Round function. Also Modified making userSelection() more concise

--- myscripts.js
+++ myscripts.js
@@ -3,17 +3,14 @@ function computerPlay(){
     switch(Math.floor(Math.random() * 3) + 1){
          case 1:{
              let computerSelection = "rock";
-             //console.log(computerSelection)
-                 break;
+                break;
 
          } case 2:{
              let computerSelection = "paper";
-             //console.log(computerSelection)
                  break;
 
          } case 3:{
              let computerSelection = "scissors"
-             //console.log(computerSelection)
                  break;
 
          } default:
@@ -23,14 +20,11 @@ function computerPlay(){
  // Allows the user to make a selection input
  function userPlay(){
      
-     let playerInput = prompt("Choose a weapon", "Rock, Paper, or Scissors");
+     let playerSelection = prompt("Choose a weapon", "Rock, Paper, or Scissors").toLowerCase();
 
- // Corrects any input a user may put in to meet requirements
-     let fixInput = playerInput.toLowerCase();
-     
-     let options = ["rock","paper","scissors"]    
-         if (options.includes(fixInput)){
-             let playerSelection = fixInput;
+      let options = ["rock","paper","scissors"]    
+         if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"){
+             return playerSelection
          } else {
              alert("That was not an option dude. C'mon actually pick one");
              userPlay();
@@ -40,32 +34,41 @@ function computerPlay(){
  function playRound(playerSelection,computerSelection) {
      
  // Can evaluate all possible combinations based off of both inputs
-             if (playerSelection == computerSelection) {
+             switch (true) {
+
+                case (playerSelection == computerSelection): {
                  alert("Draw!!! Try again!");
+                 break;
 
-             } else if (playerSelection == "paper" && computerSelection == "scissors") {
+             }  case (playerSelection == "paper" && computerSelection == "scissors"): {
                  alert("You Lose! Scissors beats Paper!");
+                 break;
 
-             } else if (playerSelection == "rock" && computerSelection == "paper") {
+             }  case (playerSelection == "rock" && computerSelection == "paper"): {
                  alert("You Lose! Paper beats Rock!");
+                 break;
 
-             } else if (playerSelection == "scissors" && computerSelection == "rock") {
+             }  case (playerSelection == "scissors" && computerSelection == "rock"): {
                  alert("You Lose! Rock beats Scissors!");
+                 break;
 
-             } else if (playerSelection == "scissors" && computerSelection == "paper") {
+             }  case (playerSelection == "scissors" && computerSelection == "paper"): {
                  alert("You Win! Scissors beats Paper!");
+                 break;
 
-              } else if (playerSelection == "paper" && computerSelection == "rock") {
+              } case (playerSelection == "paper" && computerSelection == "rock"): {
                  alert("You Win! Paper beats Rock!");
+                 break;
 
-             } else if (playerSelection == "rock" && computerSelection == "scissors") {
+             }  case (playerSelection == "rock" && computerSelection == "scissors"): {
                  alert("You Win! Rock beats Scissors!");
+                 break;
          
-             } else {
+             }  default: {
                  alert("Something went wrong");
              }               
-     }
-
+    }
+}
  //Computer makes random decision input
  const playerSelection = userPlay();
  const computerSelection = computerPlay(); 
